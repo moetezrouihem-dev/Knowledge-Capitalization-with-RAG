596c83b9c6359352ded8c734b18ec0b9aa5383b8
Moetez Rouihem <moetezrouihem5@gmail.com>
2026-07-29 17:24:15 +0100

    updated the files

---
+++ config.py
@@ -0,0 +1,18 @@
+import os
+Groq_api_key = os.environ.get("GROQ_API_KEY","") 
+groq_model = os.environ.get("GROQ_MODEL")
+
+documents_directory = os.environ.get("docs_dir", "docs") 
+faiss_index_directory = os.environ.get("faiss_dir", "faiss_index")
+
+EMBEDDING_MODEL_NAME = "sentence-transformers/all-MiniLM-L6-v2" # a local embedding model ,small but fast and efficent .
+
+CHUNK_SIZE = 400 # these are only for the pdf and the word documents, the html and excel files will be chunked differently...
+CHUNK_OVERLAP = 40
+
+TOP_K = 5  #the answer might be spread across multiple chunks
+SCORE_THRESHOLD = None # i'll set this later to filter out low quality answers (but i have to test it before )
+
+DOCLING_EXTENSIONS = {".pdf", ".docx"}
+BEAUTIFULSOUP_EXTENSIONS = {".html"}
+PANDAS_EXTENSIONS = {".xlsx"}
